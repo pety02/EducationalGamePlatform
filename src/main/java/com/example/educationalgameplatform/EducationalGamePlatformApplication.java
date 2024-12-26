e129311dac6bb325ec4304f6513386c6d731f3eb
pety02 <pety02@abv.bg>
2024-12-26 17:51:14 +0200

    Initial commit

---
+++ src/main/java/com/example/educationalgameplatform/EducationalGamePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationalgameplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalGamePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalGamePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/educationalgameplatform/EducationalGamePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationalgameplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalGamePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
